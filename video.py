74734d3db694d47ff10caffc31ab5d47b1ae8e8c
maxf8185 <maxf8185@gmail.com>
2024-03-06 19:32:21 +0200

    my first bot

--- video.py
+++ video.py
@@ -1,1 +1,29 @@
-print("Hello world")
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from decouple import config
+
+bot = Bot(token=config("TOKEN"))
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("this answer for start command")
+
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Hello my dear. Glory to Ukraine")
+
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
+
